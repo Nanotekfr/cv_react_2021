acca05d9cfc07b7d5dbca3b3c8f17369ae561653
Nanotekfr <nanotek.developpeur@gmail.com>
2021-03-02 01:29:25 +0100

    create page folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-function App() {
+import Accueil from "./pages/Accueil";
+import Cv from "./pages/Cv";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import P404 from "./pages/P404";
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello React</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Accueil} />
+          <Route path="/cv" component={Cv} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/contact" component={Contact} />
+          <Route component={P404} />
+        </Switch>
+      </BrowserRouter>
+    </>
   );
-}
+};
 
 export default App;
