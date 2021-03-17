dbb6f2915da51987bdd7953650c8b979cba4f3e5
Dylan <36613221+Nanotekfr@users.noreply.github.com>
2021-03-17 17:00:39 +0100

    portfolio_page

--- src/components/portfolio/ProjetList.js
+++ src/components/portfolio/ProjetList.js
@@ -4,19 +4,52 @@ import Projet from './Projet';
 
 export default class ProjetList extends Component {
     state= {
-        projets:portfolioData
+        projets:portfolioData,
+        radios: [
+            {id: 1, valeur: "html" },
+            {id: 2, valeur: "css" },
+            {id: 3, valeur: "javascript"},
+            {id: 4, valeur: "react"},
+            {id: 5, valeur: "php"}
+        ],
+        selectedRadio: 'html'
     };
+
+    handleRadio = (event) => {
+        let radio = event.target.value;
+        this.setState({selectedRadio: radio})
+    }
+
     render() {
-        let {projets} = this.state;
+        let {projets, radios, selectedRadio} = this.state;
+
         return (
             <div className="portfolioContent">
                 <ul className="radioDisplay">
-
+                    {
+                        radios.map((radio) => {
+                            return (
+                                <li key={radio.id}>
+                                    <input
+                                        type="radio"
+                                        name="radio"
+                                        checked={radio.value === selectedRadio}
+                                        value={radio.value}
+                                        id={radio.value}
+                                        onChange={this.handleRadio}
+                                    />
+                                    <label htmlFor={radio.value}>{radio.value}</label>
+                                </li>
+                            )
+                        })
+                    }
                 </ul>
 
                 <div className="projets">
                     {
-                        projets.map(item => {
+                        projets
+                        .filter(item => item.langages.includes(selectedRadio))
+                        .map(item => {
 
                             return (
                                 <Projet
